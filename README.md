b87843451d5a88c8996914d54f02db9151325e03
karol <01158930@pw.edu.pl>
2024-03-14 22:36:18 +0100

    working BME680

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Hydroponika
 
-- wg. instrukcji [CircutPython](https://learn.adafruit.com/circuitpython-libraries-on-micropython-using-the-raspberry-pi-pico/installing-blinka-and-libraries)
+-  CircutPython używamy [wg. instrukcji](https://learn.adafruit.com/circuitpython-libraries-on-micropython-using-the-raspberry-pi-pico/installing-blinka-and-libraries). Należy pobrać bundle z biblitekami `.py`, rozpakować i do folderu `lib` przenosić tylko potrzebne biblioteki.
+- Jeśli chcemy usunąć bibliotekę to trzeba ją też usunąć z pamięci urządenia. Jeśli chcemy dodać bibliotekę to trzeba ją wgrać na urządzenie. Czasami po wgraniu biblioteki na urządzenie trzeba zrobić reset urządzenia.
--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import time
 import board
 import busio
 from adafruit_bme280 import basic as adafruit_bme280
+import adafruit_bme680
 
 # Create sensor object, using the board's default I2C bus.
 i2c = busio.I2C(board.GP1, board.GP0)  # SCL, SDA
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c=i2c, address=0x77)
+bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c=i2c, address=0x76)
 
 # OR create sensor object, using the board's default SPI bus.
 # spi = busio.SPI(board.GP2, MISO=board.GP0, MOSI=board.GP3)
@@ -17,7 +19,9 @@ bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c=i2c, address=0x77)
 
 # change this to match the location's pressure (hPa) at sea level
 bme280.sea_level_pressure = 1013.25
+bme680.sea_level_pressure = 1013.25
 
 while True:
     print(f"\nBME280: {round(bme280.temperature, 2)}C, {round(bme280.relative_humidity, 2)}%, {round(bme280.pressure, 2)}hPa, {round(bme280.altitude, 2)}m")
+    print(f"BME680: {round(bme680.temperature, 2)}C, {round(bme680.relative_humidity, 2)}%, {round(bme680.pressure, 2)}hPa, {round(bme680.altitude, 2)}m, {bme680.gas}ohm")
     time.sleep(2)
