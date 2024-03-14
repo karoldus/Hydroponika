7593beff8a2f1a5993528069460dbd598fb35e9b
karol <01158930@pw.edu.pl>
2024-03-14 22:40:44 +0100

    refactor main

--- main.py
+++ main.py
@@ -6,22 +6,19 @@ import board
 import busio
 from adafruit_bme280 import basic as adafruit_bme280
 import adafruit_bme680
+from machine import Pin
+
+led_pin = Pin("LED", Pin.OUT)
 
 # Create sensor object, using the board's default I2C bus.
 i2c = busio.I2C(board.GP1, board.GP0)  # SCL, SDA
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c=i2c, address=0x77)
 bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c=i2c, address=0x76)
-
-# OR create sensor object, using the board's default SPI bus.
-# spi = busio.SPI(board.GP2, MISO=board.GP0, MOSI=board.GP3)
-# bme_cs = digitalio.DigitalInOut(board.GP1)
-# bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, bme_cs)
-
-# change this to match the location's pressure (hPa) at sea level
 bme280.sea_level_pressure = 1013.25
 bme680.sea_level_pressure = 1013.25
 
 while True:
+    led_pin.toggle()
     print(f"\nBME280: {round(bme280.temperature, 2)}C, {round(bme280.relative_humidity, 2)}%, {round(bme280.pressure, 2)}hPa, {round(bme280.altitude, 2)}m")
     print(f"BME680: {round(bme680.temperature, 2)}C, {round(bme680.relative_humidity, 2)}%, {round(bme680.pressure, 2)}hPa, {round(bme680.altitude, 2)}m, {bme680.gas}ohm")
     time.sleep(2)
