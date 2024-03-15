ecad87282a8554543048771bdfbf1579364f0697
2tOrka <161347086+2tOrka@users.noreply.github.com>
2024-03-15 15:06:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 import board
 import busio
+import utime
 from machine import Pin
 
 # from adafruit_bme280 import basic as adafruit_bme280
@@ -11,8 +12,14 @@ I2C_0_SCL_PIN = board.GP1
 I2C_1_SDA_PIN = board.GP2
 I2C_1_SCL_PIN = board.GP3
 # TODO add pinout...
+TRIG_PIN = 16
+ECHO_PIN = 17
+
 
 led_pin = Pin("LED", Pin.OUT)
+trigger = Pin(TRIG_PIN, Pin.OUT)
+echo = Pin(ECHO_PIN, Pin.IN, Pin.PULL_DOWN)
+
 
 # BME280 and BME680 sensors
 # i2c = busio.I2C(board.GP1, board.GP0)  # SCL, SDA
@@ -26,3 +33,26 @@ while True:
     # print(f"\nBME280: {round(bme280.temperature, 2)}C, {round(bme280.relative_humidity, 2)}%, {round(bme280.pressure, 2)}hPa, {round(bme280.altitude, 2)}m")
     # print(f"BME680: {round(bme680.temperature, 2)}C, {round(bme680.relative_humidity, 2)}%, {round(bme680.pressure, 2)}hPa, {round(bme680.altitude, 2)}m, {bme680.gas}ohm")
     time.sleep(2)
+    dist = measure_distance()
+    print("Odległość:", dist, "cm")
+    
+def measure_distance():
+    # Wysyłamy impuls TRIG przez 10 mikrosekund
+    trigger.low()
+    utime.sleep_us(2)
+    trigger.high()
+    utime.sleep_us(10)
+    trigger.low()
+    while echo.value() == 0:    
+     pass
+
+    start_time = utime.ticks_us()
+
+    while echo.value() == 1:
+        pass
+        
+    end_time = utime.ticks_us()
+    duration = utime.ticks_diff(end_time, start_time)
+    distance = duration / 58
+
+    return distance
