5c0fc5a98cf39f00680f3a3f9d00ad768605aacf
karol <01158930@pw.edu.pl>
2024-04-09 22:39:41 +0200

    new libs - hcsr04, tsl2591

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import busio
 import utime
 from machine import Pin
 
+# from adafruit_hcsr04 import HCSR04
+
 # from adafruit_bme280 import basic as adafruit_bme280
 # import adafruit_bme680
 
@@ -12,13 +14,16 @@ I2C_0_SCL_PIN = board.GP1
 I2C_1_SDA_PIN = board.GP2
 I2C_1_SCL_PIN = board.GP3
 # TODO add pinout...
-TRIG_PIN = 16
-ECHO_PIN = 17
+TRIG_PIN = board.GP6
+ECHO_PIN = board.GP7
 
 
 led_pin = Pin("LED", Pin.OUT)
-trigger = Pin(TRIG_PIN, Pin.OUT)
-echo = Pin(ECHO_PIN, Pin.IN, Pin.PULL_DOWN)
+# trigger = Pin(TRIG_PIN, Pin.OUT)
+# echo = Pin(ECHO_PIN, Pin.IN, Pin.PULL_DOWN)
+
+# HC-SR04 sensor
+# sonar = HCSR04(trigger_pin=TRIG_PIN, echo_pin=ECHO_PIN)
 
 
 # BME280 and BME680 sensors
@@ -28,30 +33,37 @@ echo = Pin(ECHO_PIN, Pin.IN, Pin.PULL_DOWN)
 # bme280.sea_level_pressure = 1013.25
 # bme680.sea_level_pressure = 1013.25
 
+# def measure_distance():
+#     trigger.low()
+#     utime.sleep_us(2)
+#     trigger.high()
+#     utime.sleep_us(10)
+#     trigger.low()
+#     while echo.value() == 0:    
+#      pass
+
+#     start_time = utime.ticks_us()
+
+#     while echo.value() == 1:
+#         pass
+        
+#     end_time = utime.ticks_us()
+#     duration = utime.ticks_diff(end_time, start_time)
+#     distance = duration / 58
+
+#     return distance
+
 while True:
     led_pin.toggle()
     # print(f"\nBME280: {round(bme280.temperature, 2)}C, {round(bme280.relative_humidity, 2)}%, {round(bme280.pressure, 2)}hPa, {round(bme280.altitude, 2)}m")
     # print(f"BME680: {round(bme680.temperature, 2)}C, {round(bme680.relative_humidity, 2)}%, {round(bme680.pressure, 2)}hPa, {round(bme680.altitude, 2)}m, {bme680.gas}ohm")
+    # try:
+    #     print((sonar.distance,))
+    # except RuntimeError:
+    #     print("Retrying!")
+    #     pass
     time.sleep(2)
-    dist = measure_distance()
-    print("Odległość:", dist, "cm")
+    # dist = measure_distance()
+    # print("Odległość:", dist, "cm")
     
-def measure_distance():
-    trigger.low()
-    utime.sleep_us(2)
-    trigger.high()
-    utime.sleep_us(10)
-    trigger.low()
-    while echo.value() == 0:    
-     pass
 
-    start_time = utime.ticks_us()
-
-    while echo.value() == 1:
-        pass
-        
-    end_time = utime.ticks_us()
-    duration = utime.ticks_diff(end_time, start_time)
-    distance = duration / 58
-
-    return distance
